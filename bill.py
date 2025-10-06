294a599ff188431dccebc9dab7fdb8b82bed411f
deepajadhav1107 <deepajadhav2001@gmail.com>
2025-10-06 17:38:55 +0530

    save

--- bill.py
+++ bill.py
@@ -2,3 +2,8 @@ units=float(input("Enter the number of units consumed:"))
 rate = 5
 bill=units*5
 print("Electricity bill is: Rs",bill)
+if bill>1000
+  discount =bill*0.10
+  bill = bill-discount
+  print("A 10% discount has been appiled.")
+  print("Final Electricity bill is:Rs",bill")
